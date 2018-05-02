b65b7b7530508c2f4c73aed54acb04de2aaf88ea
THOMAS JOHN A. GULISAO <Thomas@syntacticsinc.local>
2018-05-02 09:28:48 +0800

    Adjust header color on article cards

--- resources/views/components/article-card.blade.php
+++ resources/views/components/article-card.blade.php
@@ -1,12 +1,13 @@
 <div class="card mb-md-3">
     <div class="card-header">
-        <div class="d-flex justify-content-between">
+        <div>
             <h4><a href="{{ $link }}">{{ $title }}</a></h4>
-            <small> <i class="fa fa-clock-o"></i> {{ $published_time }}</small>
         </div>
-        <small><i class="fa fa-user-o"></i> {{ $author }}</small>
+        <small class="d-flex justify-content-between">
+            <span><i class="fa fa-user-o"></i> {{ $author }}</span>
+            <span><i class="fa fa-clock-o"></i> {{ $published_time }}</span>
+        </small>
     </div>
-
     <div class="card-body">
         {{ $content }}
     </div>
